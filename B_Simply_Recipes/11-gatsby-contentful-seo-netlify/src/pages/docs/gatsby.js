295b1ce6974ceb57b21f42f49a5de9d05b43b797
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-02 10:59:07 +0100

    11: removed docs

--- B_Simply_Recipes/11-gatsby-contentful-seo-netlify/src/pages/docs/gatsby.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "gatsby";
-const gatsby = () => {
-    return (React.createElement("div", null,
-        React.createElement("h2", null, "Documentation about Gatsby.js"),
-        React.createElement(Link, { to: "/" }, "Home")));
-};
-export default gatsby;
--- B_Simply_Recipes/11-gatsby-contentful-seo-netlify/src/pages/docs/next.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const Nextjs = () => {
-    return (React.createElement("div", null,
-        React.createElement("h2", null, "Documentation about Next.js")));
-};
-export default Nextjs;
