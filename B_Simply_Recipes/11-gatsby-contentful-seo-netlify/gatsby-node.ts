088a5b28d8f41dac483076fd76a645aa2a0c626e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-06 00:23:47 +0100

    tagTemplate + dynamic tag page generation in "gatsby-node"

--- B_Simply_Recipes/11-gatsby-contentful-seo-netlify/gatsby-node.ts
+++ B_Simply_Recipes/11-gatsby-contentful-seo-netlify/gatsby-node.ts
@@ -1,4 +1,7 @@
 import { GatsbyNode } from "gatsby";
+import path from "path";
+import slugify from "slugify";
+import { setupTags } from "./src/utils/setupTag";
 
 export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] =
   ({ actions }) => {
@@ -12,3 +15,50 @@ export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] 
     }
   `);
   };
+
+//--------------------------------------------------------------
+
+type TypeGetRecipesGatsbyNode = {
+  allContentfulSimpleRecipesGatsbyJohnSmilga: {
+    nodes: Array<{
+      content: {
+        tags: Array<string>;
+      };
+    }>;
+  };
+};
+
+export const createPages: GatsbyNode["createPages"] = async ({
+  graphql,
+  actions,
+}) => {
+  const { data, errors } = await graphql<TypeGetRecipesGatsbyNode>(`
+    query GetRecipes {
+      allContentfulSimpleRecipesGatsbyJohnSmilga {
+        nodes {
+          content {
+            tags
+          }
+        }
+      }
+    }
+  `);
+
+  if (errors) {
+    throw new Error(errors.join(", "));
+  }
+
+  if (data) {
+    data.allContentfulSimpleRecipesGatsbyJohnSmilga.nodes.forEach((recipe) => {
+      return recipe.content.tags.forEach((tag) => {
+        return actions.createPage({
+          path: "/tags/" + slugify(tag, { lower: true, trim: true }),
+          component: path.resolve("./src/templates/tagTemplate.tsx"),
+          context: {
+            tag,
+          },
+        });
+      });
+    });
+  }
+};
