4d58d75fde2c35595301736397d1198096bd079d
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-06 15:18:23 +0100

    12: cleaned up gatsby-config

--- B_Simply_Recipes/12-gatsby-contentful-seo-netlify-continuous-deployment/gatsby-config.ts
+++ B_Simply_Recipes/12-gatsby-contentful-seo-netlify-continuous-deployment/gatsby-config.ts
@@ -17,34 +17,6 @@ const config: GatsbyConfig = {
     image: "/mainImg.png",
     linkedInUsername: "daniel-b-ouattara-677146ab",
     twitterUsername: "@danielOuattara",
-
-    simpleData: [`item 1`, `item 2`],
-    complexData: [
-      {
-        id: 1,
-        first_name: `Katlin`,
-        last_name: `Sudy`,
-        email: `ksudy0@github.io`,
-        gender: `Genderqueer`,
-        ip_address: `28.40.93.31`,
-      },
-      {
-        id: 2,
-        first_name: `Baryram`,
-        last_name: `Rubinovitch`,
-        email: `brubinovitch1@mapquest.com`,
-        gender: `Male`,
-        ip_address: `82.141.109.32`,
-      },
-      {
-        id: 3,
-        first_name: `Julius`,
-        last_name: `Hue`,
-        email: `jhue2@bandcamp.com`,
-        gender: `Male`,
-        ip_address: `145.143.44.46`,
-      },
-    ],
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -70,13 +42,6 @@ const config: GatsbyConfig = {
       options: {
         name: `styling`, // The unique name for each instance
         path: `${__dirname}/src/assets/css`, // Path to the directory
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `examples`, // The unique name for each instance
-        path: `${__dirname}/src/examples-images`, // Path to the directory
       },
     },
     {
