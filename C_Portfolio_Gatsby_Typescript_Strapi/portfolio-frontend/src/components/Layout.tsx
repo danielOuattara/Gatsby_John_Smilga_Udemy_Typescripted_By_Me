ee74096a86737e5a2c0d10da3d0da736238ed849
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-06 22:46:43 +0100

    C:Portfolio: adding global Layout in gatsby-browser and gatsby-ssr

--- C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/components/Layout.tsx
+++ C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/components/Layout.tsx
@@ -1,8 +1,15 @@
+// Layout.tsx
+
 import * as React from "react";
 import { PageProps } from "gatsby";
 import { Navbar, Sidebar, Footer } from "./index";
 import "../assets/css/main.css";
 
-export default function Layout({ children }: PageProps) {
-  return <>{children}</>;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
 }
