d41eab4b90c93f34993fe7c8dea91b9bb691d01c
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-06 23:11:03 +0100

    C:Portfolio: Nav comp + module decla for svg & all image

--- C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/components/Navbar.tsx
+++ C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/components/Navbar.tsx
@@ -5,5 +5,24 @@ import { page_links } from "../constants";
 import { Link } from "gatsby";
 
 export default function Navbar() {
-  return <h2>navbar component</h2>;
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo as never as string} alt="web dev" />
+          <button className="toggle-btn">
+            <FaAlignRight />
+          </button>
+        </div>
+
+        <div className="nav-links">
+          {page_links.map((link) => (
+            <Link key={link.id} to={link.url}>
+              {link.text}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
 }
