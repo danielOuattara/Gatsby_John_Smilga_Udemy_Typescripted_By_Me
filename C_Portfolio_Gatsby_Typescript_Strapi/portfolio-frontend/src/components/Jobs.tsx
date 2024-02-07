d7f44009a246f470709b405af7b37900e569b230
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-07 19:33:41 +0100

    C:Portfolio: render Jobs experiences

--- C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/components/Jobs.tsx
+++ C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/components/Jobs.tsx
@@ -4,6 +4,77 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 import { graphql, useStaticQuery } from "gatsby";
 import { Link } from "gatsby";
 
+type TypeJobsQuery = {
+  allStrapiJob: {
+    totalCount: number;
+    nodes: Array<{
+      position: string;
+      company: string;
+      date: string;
+      descriptions: Array<{
+        id: string;
+        item: string;
+      }>;
+    }>;
+  };
+};
+export const query = graphql`
+  query Jobs {
+    allStrapiJob {
+      totalCount
+      nodes {
+        position
+        company
+        date
+        descriptions {
+          id
+          item
+        }
+      }
+    }
+  }
+`;
+
 export default function Jobs() {
-  return <h2>jobs component</h2>;
+  const data: TypeJobsQuery = useStaticQuery(query);
+  const jobs = data.allStrapiJob.nodes;
+  console.log(jobs);
+
+  const [jobIndex, setJobIndex] = React.useState(0);
+  const jobToShow = jobs[jobIndex];
+
+  return (
+    <section className="section jobs">
+      <Title title="experiences" />
+
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((job, index) => (
+            <button
+              key={index}
+              className={jobIndex === index ? `job-btn active-btn` : `job-btn `}
+              onClick={() => setJobIndex(index)}
+            >
+              {job.company}
+            </button>
+          ))}
+        </div>
+
+        <article className="job-info">
+          <h3>{jobToShow.position}</h3>
+          <h4>{jobToShow.company}</h4>
+          <p className="job-date">{jobToShow.date}</p>
+          {jobToShow.descriptions.map((item) => (
+            <div key={item.id} className="job-desc">
+              <FaAngleDoubleRight className="job-icon" />
+              <p>{item.item}</p>
+            </div>
+          ))}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        more info
+      </Link>
+    </section>
+  );
 }
--- C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/pages/index.tsx
+++ C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/pages/index.tsx
@@ -9,6 +9,7 @@ export default function IndexPage() {
       <main>
         <Hero />
         <Services />
+        <Jobs />
       </main>
     </>
   );
