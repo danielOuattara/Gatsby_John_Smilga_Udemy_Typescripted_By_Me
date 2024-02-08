67de363333ea3a6298a22ce88f0979f28df9d400
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-08 20:21:08 +0100

    C:Portfolio: Gatsby: rendering  Contact page

--- C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/pages/contact.tsx
+++ C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/pages/contact.tsx
@@ -2,5 +2,40 @@ import * as React from "react";
 import { SEO } from "../components";
 
 export default function Contact() {
-  return <h2>contact page</h2>;
+  return (
+    <section className="contact-page">
+      <article className="contact-form">
+        <h3>stay in touch ?</h3>
+        <p>(not activated yet)</p>
+        <form>
+          <div className="form-group">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="name"
+              className="form-control"
+            />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="email"
+              className="form-control"
+            />
+            <textarea
+              name="message"
+              id="message"
+              rows={7}
+              className="form-control"
+              placeholder="message"
+            ></textarea>
+          </div>
+          <button type="submit" className="submit-btn btn">
+            submit
+          </button>
+        </form>
+      </article>
+    </section>
+  );
 }
