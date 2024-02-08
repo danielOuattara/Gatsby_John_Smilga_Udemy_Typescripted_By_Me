9bb3e3ee772fc3ccad1a3fd9d241581fe0bf60f8
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-08 18:54:13 +0100

    C:Portfolio:Gatsby:file system API: create dynamic for projects  page using project title

---
+++ C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/pages/projects/{StrapiProject.title}.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { Link, PageProps, graphql } from "gatsby";
+import { SEO } from "./../../components";
+
+export const query = graphql`
+  query ProjectTemplate($title: String) {
+    strapiProject(title: { eq: $title }) {
+      title
+      web_url
+      description
+      image {
+        localFile {
+          publicURL
+        }
+      }
+    }
+  }
+`;
+
+type TypeProjectTemplateQuery = {
+  strapiProject: {
+    title: string;
+    web_url: string;
+    description: string;
+    image: {
+      localFile: { publicURL: string };
+    };
+  };
+};
+
+type TypePageContext = {
+  title: string;
+};
+
+export default function ProjectTemplate({
+  data,
+  pageContext,
+}: PageProps<TypeProjectTemplateQuery, TypePageContext>) {
+  const projectData = data.strapiProject;
+  return (
+    <main className="project-template-page">
+      <h2>{pageContext.title}</h2>
+      <p>{projectData.description}</p>
+      <a href={projectData.web_url} target="_blank" rel="noreferrer">
+        <img
+          src={projectData.image.localFile.publicURL}
+          alt={projectData.title}
+        />
+      </a>
+      <Link to="/projects" className="btn">
+        back to projects
+      </Link>
+    </main>
+  );
+}
