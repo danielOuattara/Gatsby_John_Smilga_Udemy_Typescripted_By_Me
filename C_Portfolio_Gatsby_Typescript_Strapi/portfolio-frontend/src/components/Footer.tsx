4acc99d7f46efa9040201263c597e1fc2ff205f3
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-08 20:06:25 +0100

    C:Portfolio: Gatsby: rendering  Footer

--- C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/components/Footer.tsx
+++ C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/components/Footer.tsx
@@ -2,5 +2,21 @@ import * as React from "react";
 import { social_links } from "../constants";
 
 export default function Footer() {
-  return <h2>footer component</h2>;
+  return (
+    <footer className="footer">
+      <div>
+        <div className="footer-links social-links">
+          {social_links.map((link) => (
+            <a href={link.url} key={link.id} className="social-link">
+              {link.icon}
+            </a>
+          ))}
+        </div>
+        <h4>
+          copyright &copy; 2019 - {new Date().getFullYear()}{" "}
+          <span>Web dev</span> all right reserved
+        </h4>
+      </div>
+    </footer>
+  );
 }
--- C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/components/Layout.tsx
+++ C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/src/components/Layout.tsx
@@ -10,6 +10,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <Navbar />
       {children}
+      <Footer />
     </>
   );
 }
