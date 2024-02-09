c90e40cd8570de2c1eadf87ba3cc279c301f702e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-09 14:31:10 +0100

    C:Portfolio: Gatsby: update data in SiteMetadata

--- C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/gatsby-config.ts
+++ C_Portfolio_Gatsby_Typescript_Strapi/portfolio-frontend/gatsby-config.ts
@@ -5,8 +5,19 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `portfolio-frontend`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "Fullstack Portfolio",
+    description:
+      "Portfolio website where I present myself and show my skills on different kind of projects and using a wide range of effective web design technologies. From the contact page, everybody can contact me",
+    inspiredBy: `John Smilga`,
+    author: {
+      name: `daniel.ouattara`,
+      place: `Combs la ville, France`,
+    },
+    titleTemplate: `%s | Fullstack Portfolio `,
+    siteUrl: "https://daniel-gatsby-ts-strapi-portfolio.netlify.app",
+    image: "/home.png",
+    linkedInUsername: "daniel-b-ouattara-677146ab",
+    twitterUsername: "@danielOuattara",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
