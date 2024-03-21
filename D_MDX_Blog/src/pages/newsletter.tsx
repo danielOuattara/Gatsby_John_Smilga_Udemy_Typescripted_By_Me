9347b2a81a37bf5de5bc74c6806375973c48571b
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-21 19:27:54 +0100

    D:Mdx Blog: logic for Newsletter

--- D_MDX_Blog/src/pages/newsletter.tsx
+++ D_MDX_Blog/src/pages/newsletter.tsx
@@ -3,5 +3,40 @@ import { Link, HeadFC, PageProps } from "gatsby";
 import Layout from "../components/Layout";
 
 export default function NewsLetter() {
-  return <h4>newsletter</h4>;
+  return (
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Get all latest stories to your inbox</h2>
+          <h4>I write to my friends every few weeks</h4>
+          <form
+            className="contact-form"
+            name="contact"
+            method="post"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+            action="/success"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="form-control"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              className="form-control"
+            />
+            <button type="submit" className="form-control btn">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
+  );
 }
