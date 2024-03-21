16b062f87b76aa78e14222738a9382094046cbea
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-21 18:29:09 +0100

    D:Mdx Blog: logic for Footer

--- D_MDX_Blog/src/components/Footer.tsx
+++ D_MDX_Blog/src/components/Footer.tsx
@@ -2,5 +2,14 @@ import * as React from "react";
 import SocialLinks from "../constants/socialLinks";
 
 export default function Footer() {
-  return <h4>footer component</h4>;
+  return (
+    <footer className="footer">
+      <div>
+        <SocialLinks styleClass={"footer-icons"} />
+        <p>
+          &copy; 2019 - {new Date().getFullYear()} mdx blog. all rights reserved
+        </p>
+      </div>
+    </footer>
+  );
 }
