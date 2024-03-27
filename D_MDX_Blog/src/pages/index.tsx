24e5d029140e60cb4f9a876c65326acd150c07e0
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-27 08:57:18 +0100

    D:Mdx Blog: Home page: query featured blogs: the last 3

--- D_MDX_Blog/src/pages/index.tsx
+++ D_MDX_Blog/src/pages/index.tsx
@@ -2,13 +2,61 @@ import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import { Hero, Layout, Posts } from "../components";
 import { graphql } from "gatsby";
+import { IGatsbyImageData } from "gatsby-plugin-image";
 
-export default function IndexPage() {
+export default function IndexPage({ data }: PageProps<TypeFeaturedBlogsQuery>) {
+  console.log(data.allMdx.nodes);
   return (
     <Layout>
       <Hero showPersonImg={true} />
     </Layout>
   );
 }
+
+type TypeFeaturedBlogsQuery = {
+  allMdx: {
+    nodes: Array<{
+      id: string;
+      excerpt: string;
+      frontmatter: {
+        title: string;
+        author: string;
+        category: string;
+        readTime: number;
+        slug: string;
+        date: string;
+        image: {
+          childrenImageSharp: Array<{
+            gatsbyImageData: IGatsbyImageData;
+          }>;
+        };
+      };
+    }>;
+  };
+};
+
+export const query = graphql`
+  query FeaturedBlogs {
+    allMdx(limit: 3, sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM Do, YYYY")
+          image {
+            childrenImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`;
 
 export const Head: HeadFC = () => <title>Home Page</title>;
