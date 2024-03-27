46891055591078a34727f839e7e8c90af505a2fe
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-27 19:55:20 +0100

    D:Mdx Blog:  BannerRecent  TSX logic

--- D_MDX_Blog/src/components/Banner/Recent.tsx
+++ D_MDX_Blog/src/components/Banner/Recent.tsx
@@ -2,12 +2,72 @@ import * as React from "react";
 import styled from "styled-components";
 import { graphql, useStaticQuery } from "gatsby";
 import { Link } from "gatsby";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage, IGatsbyImageData } from "gatsby-plugin-image";
 import Title from "./Title";
 
 export default function Recent() {
-  return <Wrapper>Banner Recent</Wrapper>;
+  const data: TypeBannerRecentPostsQuery = useStaticQuery(query);
+  return (
+    <Wrapper>
+      <Title title={"recent posts"} />
+      {data.allMdx.nodes.map((post) => (
+        <Link
+          to={`/posts/${post.frontmatter.slug}`}
+          className="post"
+          key={post.id}
+        >
+          <GatsbyImage
+            image={getImage(post.frontmatter.image) as IGatsbyImageData}
+            alt={post.frontmatter.title}
+            className="img"
+          />
+          <div>
+            <h5>{post.frontmatter.title}</h5>
+            <p>{post.frontmatter.date}</p>
+          </div>
+        </Link>
+      ))}
+    </Wrapper>
+  );
 }
+
+type TypeBannerRecentPostsQuery = {
+  allMdx: {
+    nodes: Array<{
+      id: string;
+      frontmatter: {
+        title: string;
+        slug: string;
+        date: string;
+        image: {
+          childImageSharp: {
+            gatsbyImageData: IGatsbyImageData;
+          };
+        };
+      };
+    }>;
+  };
+};
+
+const query = graphql`
+  query BannerRecentPosts {
+    allMdx(limit: 5, sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          slug
+          date(formatString: "MMMM Do, YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Wrapper = styled.div`
   .post {
