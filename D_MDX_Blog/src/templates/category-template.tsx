69d8d90d94b62d4260469234a7b0bbf847680345
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-27 16:15:26 +0100

    D:Mdx Blog: add logic for querying in category-template page

--- D_MDX_Blog/src/templates/category-template.tsx
+++ D_MDX_Blog/src/templates/category-template.tsx
@@ -1,7 +1,70 @@
 import * as React from "react";
 import { Layout, Hero, Posts } from "../components/";
-import { graphql } from "gatsby";
+import { graphql, PageProps } from "gatsby";
+import { IGatsbyImageData } from "gatsby-plugin-image";
 
-export default function CategoryTemplate() {
-  return <h2>category template</h2>;
+type TypePageContext = {
+  category: string;
+};
+
+export default function CategoryTemplate({
+  data,
+  pageContext,
+}: PageProps<TypeSingleCategoryQuery, TypePageContext>) {
+  return (
+    <Layout>
+      <Hero showPersonImg={true} />
+      <Posts
+        title={`categories / ${pageContext.category}`}
+        posts={data.allMdx.nodes}
+      />
+    </Layout>
+  );
 }
+
+type TypeSingleCategoryQuery = {
+  allMdx: {
+    nodes: Array<{
+      id: string;
+      excerpt: string;
+      frontmatter: {
+        title: string;
+        author: string;
+        category: string;
+        readTime: number;
+        slug: string;
+        date: string;
+        image: {
+          publicURL: string;
+          childImageSharp: {
+            gatsbyImageData: IGatsbyImageData;
+          };
+        };
+      };
+    }>;
+  };
+};
+
+export const query = graphql`
+  query SingleCategory($category: String) {
+    allMdx(filter: { frontmatter: { category: { eq: $category } } }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+            publicURL
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`;
