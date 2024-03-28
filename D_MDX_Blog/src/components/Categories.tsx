50b50268b54d6634a3b33fab25953a8f3f0d9c8e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-28 04:51:32 +0100

    D:Mdx Blog:  BannerCategories & Categories  TSX logic

--- D_MDX_Blog/src/components/Categories.tsx
+++ D_MDX_Blog/src/components/Categories.tsx
@@ -2,6 +2,32 @@ import * as React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import { Link } from "gatsby";
 
+type TypeAllPostsByCategory = {
+  allMdx: { distinct: Array<string> };
+};
+
+const query = graphql`
+  query AllPostsByCategory {
+    allMdx {
+      distinct(field: { frontmatter: { category: SELECT } })
+    }
+  }
+`;
+
 export default function Categories() {
-  return <h4>categories component</h4>;
+  const data: TypeAllPostsByCategory = useStaticQuery(query);
+  return (
+    <ul className="categories">
+      {data.allMdx.distinct.map((category, index) => (
+        <li key={index}>
+          <Link
+            className="category"
+            to={`/categories/${category.toLowerCase()}`}
+          >
+            {category}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
