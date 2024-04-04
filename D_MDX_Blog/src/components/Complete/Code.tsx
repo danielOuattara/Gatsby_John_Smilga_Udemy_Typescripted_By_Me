24a8f083c9bc7f4377fdb5f7f6b9bc2a93b5023e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-04 14:45:27 +0200

    D:Mdx Blog: Code.tsx comp. : logic to handle both 'code' and 'inlineCode' error styling

--- D_MDX_Blog/src/components/Complete/Code.tsx
+++ D_MDX_Blog/src/components/Complete/Code.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
-import type { PageProps } from "gatsby";
 
-export default function Code({ children }: PageProps) {
+interface ICodeProps {
+  children?: React.ReactNode;
+}
+
+export default function Code({ children }: ICodeProps) {
   return (
     <code
       style={{
@@ -15,3 +18,34 @@ export default function Code({ children }: PageProps) {
     </code>
   );
 }
+
+//-------------------------------------------------------------
+
+// import * as React from "react";
+
+// interface ICodeProps {
+//   children?: React.ReactNode;
+// }
+
+// export default function Code({ children }: ICodeProps) {
+//   const isCodeBlock =
+//     typeof children === "string" && children.trim().startsWith("```");
+
+//   return isCodeBlock ? (
+//     <pre className="language-tsx">
+//       <code>{children}</code>
+//     </pre>
+//   ) : (
+//     <code
+//       style={{
+//         background: "var(--clr-grey-10)",
+//         color: "var(--clr-grey-5)",
+//         padding: "1rem",
+//         borderRadius: "var(--radius)",
+//         margin: "1rem auto 1rem auto",
+//       }}
+//     >
+//       {children}
+//     </code>
+//   );
+// }
